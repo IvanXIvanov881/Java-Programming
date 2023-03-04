42d28f8457cb8f6187637876166b80b399f1890d
IvanXIvanov881 <vankok8812@gmail.com>
2023-03-04 13:05:26 +0200

    PB7

---
+++ PSJava/src/PB7/Numbersfrom1to100.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Numbersfrom1to100 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        for  ( int a = 1; a <= 100; a++) {
+            System.out.println(a);
+        }
+    }
+}
