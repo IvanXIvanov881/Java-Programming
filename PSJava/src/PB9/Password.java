91df4416a7221abbe35537d388d929196eb1ef94
IvanXIvanov881 <vankok8812@gmail.com>
2023-03-14 23:27:16 +0200

    PB9

---
+++ PSJava/src/PB9/Password.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Password {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String username = scanner.nextLine();
+        String password = scanner.nextLine();
+
+        while (true) {
+            String password1 = scanner.nextLine();
+            if (password1.equals(password)) {
+               System.out.printf("Welcome %s!", username);
+               break;
+            }
+        }
+    }
+}
