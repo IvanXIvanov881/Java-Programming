70b4b77a0b9b43e075c5394b897dc0baf4339b3c
IvanXIvanov881 <vankok8812@gmail.com>
2023-03-24 18:23:47 +0200

    PB11

---
+++ PSJava/src/PB11/EqualSumsEvenOddPosition.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class EqualSumsEvenOddPosition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Напишете програма, която чете от конзолата две шестцифрени цели числа в диапазона от 100000 до 300000.
+        // Винаги първото въведено число ще бъде по малко от второто. На конзолата да се отпечатат на 1 ред разделени с интервал всички числа,
+        // които се намират между двете, прочетени от конзолата числа и отговарят на следното условие:
+
+        //· сумата от цифрите на четни и нечетни позиции да са равни. Ако няма числа, отговарящи на условието на конзолата не се извежда резултат.
+
+
+        int n1 = Integer.parseInt(scanner.nextLine());
+        int n2 = Integer.parseInt(scanner.nextLine());
+
+        int correctnum = 0;
+int add = 0;
+int odd = 0;
+        for (int i = n1; i < n2; i++) {
+             correctnum = i;
+        }
+
+        while (true) {
+
+        }
+
+    }
+}
