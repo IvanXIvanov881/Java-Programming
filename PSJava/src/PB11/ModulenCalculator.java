a00d4a37ba21754e0285b5286f79ecaa9cbc97f6
IvanXIvanov881 <vankok8812@gmail.com>
2023-03-27 18:45:54 +0300

    PB11

---
+++ PSJava/src/PB11/ModulenCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ModulenCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //int n1 = Integer.parseInt(scanner.nextLine());
+        //int n2 = Integer.parseInt(scanner.nextLine());
+
+       // double total = n1 % n2;
+
+       // System.out.println(total);
+
+        int num4 = (0 % 10);
+        //if (num4 % N == 0) {
+           //counter++;
+            System.out.println(num4);
+       // }
+    }
+}
