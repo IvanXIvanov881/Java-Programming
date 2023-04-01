08b7b42272d39aa4dc07720213a2130963264beb
IvanXIvanov881 <vankok8812@gmail.com>
2023-04-01 16:27:32 +0300

    PB More

---
+++ PSJava/src/PBMore/NextYear.java
@@ -0,0 +1,15 @@
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class NextYear {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Calendar calendar = (Calendar) Calendar.getInstance();
+
+        System.out.println(calendar.getWeekYear());
+
+
+        }
+    }
+
