379c0b2711bc6a8ecf2b3cb8cb4ae34d96bc5b64
IvanXIvanov881 <vankok8812@gmail.com>
2023-08-11 18:25:10 +0300

    Create SumOfOddNumbers.java

---
+++ PSJava/src/Fundamentals/BasicSyntaxConditionalStatementsAndLoops/SumOfOddNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumOfOddNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int num = 0;
+        for (int i = 0; i <= n * 2 ; i++) {
+
+            if(i % 2 != 0){
+                System.out.println(i);
+                num = num + i;
+            }
+        }
+        System.out.println("Sum: " + num);
+    }
+}
