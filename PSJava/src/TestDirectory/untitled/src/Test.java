2342a8c4d982a9690f2cfa9adbfc9e6a562cb6d7
IvanXIvanov881 <vankok8812@gmail.com>
2023-08-16 19:01:40 +0300

    Test

---
+++ PSJava/src/TestDirectory/untitled/src/Test.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
